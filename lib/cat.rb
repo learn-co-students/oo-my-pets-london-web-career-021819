cd748211cc688389d22d8f961765f89cc3856307
mariamjaludi <mariam.jaludi@gmail.com>
2019-02-22 15:41:26 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,14 @@
 class Cat
   # code goes here
+  attr_reader :name, :species
+  attr_accessor :mood
+  def initialize(name, mood= "nervous")
+    @name = name
+    @mood = mood
+    @species = species
+  end
+
+  def say_species
+    @species
+  end
 end
